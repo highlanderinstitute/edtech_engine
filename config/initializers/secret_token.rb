fd3425af5aa15f26f24cd286a745be28868e2b8f
Highlander Institute <admin@highlanderinstitute.org>
2013-02-14 15:03:27 -0500

    generated rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdtechEngine::Application.config.secret_token = '8e57bdbc838cf66d1dabc201416f561913a8f1e1105c5c48b0a0f5918b8c64f87ff8c972bdea39d0a56ba15fe0e18cacde951d5db6676faf75516a63e5e2b503'
